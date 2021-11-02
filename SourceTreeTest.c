19e63ca20db32cba3bbbdc0aa3b83f1c5ed9618a
ParkSeongHo <qkrtjdgh8040@naver.com>
2021-11-02 17:18:11 +0900

    test 브랜치에 코드에 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,8 +1,12 @@
 ##include <stdio.h>
 
 int main(){
+  int i =0;
   printf("First oss code\n", );
   printf("Second Oss code\n", );
+  for (int i=0; i<10; i++){
+  printf("test 브랜치\n", );
+}  /* c*/
 
   return 0;
 }
